09d7c2974371f1734ac6e915f75aade6115207a1
Sharnie Ivery <sharnieivery@gmail.com>
2016-05-19 18:04:22 -0400

    removed pry and others

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,11 +1,4 @@
 class LandingController < ApplicationController
-  def index
-    client = Instagram.client(:access_token => session[:access_token])
-    @user = client.user
-    binding.pry
-    @media = client.media_popular
-  end
-
   def callback
     response = Instagram.get_access_token(params[:code], redirect_uri: ENV['REDIRECT_URI'])
     session[:access_token] = response.access_token
